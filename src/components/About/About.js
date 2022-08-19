fcc938d0e0f489f6e47c9b797b33df0af7efeeaa
Abraham Reuben P. Aguas <abraham.reuben10@gmail.com>
2022-08-19 20:57:07 +0800

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ export default function Projects() {
         <main className="about">
             <section className="about">
                 <h1 className="title">about me</h1>
-                <p>I am currently taking up my 3rd year in bachelor’s degree of Information Technology and I am focused most especially in web development career. I showcase here some of my projects and designs. Hope you’ll like it.</p>
+                <p>I am currently taking up my 4th year in bachelor’s degree of Information Technology and I am focused most especially in web development career. I showcase here some of my projects and designs. Hope you’ll like it.</p>
             </section>
             <section className="certificates">
                 <h1 className="title">certifications <BsArrow90DegDown className="icon" /></h1>
