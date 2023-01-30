86eab8716c7543f9bd76dce6b82a16861baab4aa
Abraham Reuben P. Aguas <abraham.reuben10@gmail.com>
2023-01-30 23:17:39 +0800

    update

--- src/components/About/certifications.js
+++ src/components/About/certifications.js
@@ -1,26 +1,34 @@
-export default [
-    {
+export default [{
         id: 1,
-        image_url: require('../../assets/images/cert1.png'),
-        title: "E-commerce UI Design",
-        tag: "UI DESIGN",
-        tagcolor: "#5177ff",
-        tagtext: "#fff",
+        image_url: require('../../assets/images/certificates/testdome_html.png'),
+
     },
     {
         id: 2,
-        image_url: require('../../assets/images/cert2.png'),
-        title: "Hospital Information System",
-        tag: "FULL STACK WEB APP",
-        tagcolor: "#ffaf00",
-        tagtext: "#2d2d2d"
+        image_url: require('../../assets/images/certificates/hackerrank_css.png'),
+
     },
     {
         id: 3,
-        image_url: require('../../assets/images/cert2.png'),
-        title: "Hospital Information System",
-        tag: "FULL STACK WEB APP",
-        tagcolor: "#ffaf00",
-        tagtext: "#2d2d2d"
+        image_url: require('../../assets/images/certificates/freecodecamp_javascript.png'),
+
+    },
+    {
+        id: 4,
+        image_url: require('../../assets/images/certificates/hackerrank_sql.png'),
+
+    },
+    {
+        id: 5,
+        image_url: require('../../assets/images/certificates/cognitiveclass_python101.png'),
+
     }
 ]
+//   {
+//         id: 3,
+//         image_url: require('../../assets/images/cert2.png'),
+//         title: "Hospital Information System",
+//         tag: "FULL STACK WEB APP",
+//         tagcolor: "#ffaf00",
+//         tagtext: "#2d2d2d"
+//     }
