6345bd293ffb9e44e6a11296c8f45f72ba48ded9
RA Aguas <abraham.reuben10@gmail.com>
2023-09-14 13:34:08 +0800

    Removed link CPS

--- src/components/Projects/projectlist.js
+++ src/components/Projects/projectlist.js
@@ -1,48 +1,48 @@
 export default [{
-        id: 1,
-        image_url: require('../../assets/images/project.png'),
-        title: "E-commerce UI Design",
-        tag: "UI DESIGN",
-        tagcolor: "#5177ff",
-        tagtext: "#fff",
-        link: "https://www.behance.net/gallery/143715675/E-commerce-UI-Design"
-    },
-    {
-        id: 2,
-        image_url: require('../../assets/images/project4.png'),
-        title: "Full Stack E-commerce Site",
-        tag: "FULL STACK WEB APP",
-        tagcolor: "#ffaf00",
-        tagtext: "#2d2d2d",
-        link: "https://abrahamreubenaguas-portfolio.vercel.app/projects"
-    },
-    {
-        id: 3,
-        image_url: require('../../assets/images/cpsproj.png'),
-        title: "CPS Website",
-        tag: "FULL STACK WEB APP / CMS",
-        tagcolor: "#ffaf00",
-        tagtext: "#2d2d2d",
-        link: "https://cps.neu.edu.ph/home"
+    id: 1,
+    image_url: require('../../assets/images/project.png'),
+    title: "E-commerce UI Design",
+    tag: "UI DESIGN",
+    tagcolor: "#5177ff",
+    tagtext: "#fff",
+    link: "https://www.behance.net/gallery/143715675/E-commerce-UI-Design"
+},
+{
+    id: 2,
+    image_url: require('../../assets/images/project4.png'),
+    title: "Full Stack E-commerce Site",
+    tag: "FULL STACK WEB APP",
+    tagcolor: "#ffaf00",
+    tagtext: "#2d2d2d",
+    link: "https://abrahamreubenaguas-portfolio.vercel.app/projects"
+},
+{
+    id: 3,
+    image_url: require('../../assets/images/cpsproj.png'),
+    title: "CPS Website",
+    tag: "FULL STACK WEB APP / CMS",
+    tagcolor: "#ffaf00",
+    tagtext: "#2d2d2d",
+    link: "https://abrahamreubenaguas-portfolio.vercel.app/projects"
 
-    },
-    {
-        id: 4,
-        image_url: require('../../assets/images/project2.png'),
-        title: "Hospital Information System",
-        tag: "FULL STACK WEB APP",
-        tagcolor: "#ffaf00",
-        tagtext: "#2d2d2d",
-        link: "https://abrahamreubenaguas-portfolio.vercel.app/projects"
+},
+{
+    id: 4,
+    image_url: require('../../assets/images/project2.png'),
+    title: "Hospital Information System",
+    tag: "FULL STACK WEB APP",
+    tagcolor: "#ffaf00",
+    tagtext: "#2d2d2d",
+    link: "https://abrahamreubenaguas-portfolio.vercel.app/projects"
 
-    },
-    {
-        id: 5,
-        image_url: require('../../assets/images/project3.png'),
-        title: "Food Ordering App",
-        tag: "FRONT-END / BOOTSTRAP",
-        tagcolor: "#5177ff",
-        tagtext: "#fff",
-        link: "https://ar-ey.github.io/e-commerce.github.io/"
-    }
+},
+{
+    id: 5,
+    image_url: require('../../assets/images/project3.png'),
+    title: "Food Ordering App",
+    tag: "FRONT-END / BOOTSTRAP",
+    tagcolor: "#5177ff",
+    tagtext: "#fff",
+    link: "https://ar-ey.github.io/e-commerce.github.io/"
+}
 ]
