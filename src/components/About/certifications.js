56327e3fd9f95d8517e9639c332a71b15364e818
RA Aguas <abraham.reuben10@gmail.com>
2024-02-20 15:19:20 +0800

    Added certificates

--- src/components/About/certifications.js
+++ src/components/About/certifications.js
@@ -1,34 +1,42 @@
-export default [{
+export default [
+    {
         id: 1,
-        image_url: require('../../assets/images/certificates/testdome_html.png'),
+        image_url: require('../../assets/images/certificates/HTML & CSS in depth_page-0001.jpg'),
 
     },
     {
         id: 2,
-        image_url: require('../../assets/images/certificates/hackerrank_css.png'),
+        image_url: require('../../assets/images/certificates/Introduction To Front-End Development_page-0001.jpg'),
 
     },
     {
         id: 3,
-        image_url: require('../../assets/images/certificates/freecodecamp_javascript.png'),
+        image_url: require('../../assets/images/certificates/Programming with JavaScript_page-0001.jpg'),
 
     },
     {
         id: 4,
-        image_url: require('../../assets/images/certificates/hackerrank_sql.png'),
+        image_url: require('../../assets/images/certificates/React Basics_page-0001.jpg'),
 
     },
     {
         id: 5,
+        image_url: require('../../assets/images/certificates/Version Control_page-0001.jpg'),
+
+    },
+    {
+        id: 6,
+        image_url: require('../../assets/images/certificates/freecodecamp_javascript.png'),
+
+    },
+    {
+        id: 7,
+        image_url: require('../../assets/images/certificates/hackerrank_sql.png'),
+
+    },
+    {
+        id: 8,
         image_url: require('../../assets/images/certificates/cognitiveclass_python101.png'),
 
     }
 ]
-//   {
-//         id: 3,
-//         image_url: require('../../assets/images/cert2.png'),
-//         title: "Hospital Information System",
-//         tag: "FULL STACK WEB APP",
-//         tagcolor: "#ffaf00",
-//         tagtext: "#2d2d2d"
-//     }
