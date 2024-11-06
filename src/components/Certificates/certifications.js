6c526ee6a777f61cdf7a68032e5d22c5e813561f
Abraham Reuben Aguas <abraham.reuben10@gmail.com>
2024-11-07 00:00:41 +0800

    Added AZ900 certificate

--- src/components/Certificates/certifications.js
+++ src/components/Certificates/certifications.js
@@ -3,6 +3,14 @@ export default [
 
     {
         id: 1,
+        image_url: require('../../assets/images/certificates/AZ-900.jpg'),
+        tag: "AZ-900",
+        tagcolor: "#5177ff",
+        tagtext: "#fff",
+        link: "https://learn.microsoft.com/api/credentials/share/en-us/AbrahamReubenPAguas-6160/4906C5DC21BB4DAB?sharingId=E2C0D526310753FD"
+    },
+    {
+        id: 2,
         image_url: require('../../assets/images/certificates/React Basics_page-0001.jpg'),
         tag: "React Basics",
         tagcolor: "#5177ff",
@@ -10,7 +18,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/records/T866ZDH92VRT"
     },
     {
-        id: 2,
+        id: 3,
         image_url: require('../../assets/images/certificates/HTML & CSS in depth_page-0001.jpg'),
         tag: "HTML & CSS In Depth",
         tagcolor: "#5177ff",
@@ -18,7 +26,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/records/9MZAA3CK29EE"
     },
     {
-        id: 3,
+        id: 4,
         image_url: require('../../assets/images/certificates/Introduction To Back-End Development_page-0001.jpg'),
         tag: "Back-End Development",
         tagcolor: "#5177ff",
@@ -26,7 +34,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/records/VXF769XFZ3AB"
     },
     {
-        id: 4,
+        id: 5,
         image_url: require('../../assets/images/certificates/Programming with JavaScript_page-0001.jpg'),
         tag: "Programming with JavaScript",
         tagcolor: "#5177ff",
@@ -34,7 +42,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/verify/ACMDQCF34QER"
     },
     {
-        id: 5,
+        id: 6,
         image_url: require('../../assets/images/certificates/Version Control_page-0001.jpg'),
         tag: "Version Control",
         tagcolor: "#5177ff",
@@ -42,7 +50,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/records/42XQW44USBEK"
     },
     {
-        id: 6,
+        id: 7,
         image_url: require('../../assets/images/certificates/Introduction To Front-End Development_page-0001.jpg'),
         tag: "Front-End Development",
         tagcolor: "#5177ff",
@@ -50,7 +58,7 @@ export default [
         link: "https://www.coursera.org/account/accomplishments/records/SVLZXX9Z7XYM"
     },
     {
-        id: 7,
+        id: 8,
         image_url: require('../../assets/images/certificates/freecodecamp_javascript.png'),
         tag: "JavaScript DSA",
         tagcolor: "#5177ff",
@@ -58,7 +66,7 @@ export default [
         link: "https://freecodecamp.org/certification/fcccaff056c-c669-422f-a2b0-70431cb8da4d/javascript-algorithms-and-data-structures"
     },
     {
-        id: 8,
+        id: 9,
         image_url: require('../../assets/images/certificates/hackerrank_sql.png'),
         tag: "SQL",
         tagcolor: "#5177ff",
@@ -66,7 +74,7 @@ export default [
         link: "https://www.hackerrank.com/certificates/e20af696f075"
     },
     {
-        id: 9,
+        id: 10,
         image_url: require('../../assets/images/certificates/cognitiveclass_python101.png'),
         tag: "Python 101",
         tagcolor: "#5177ff",
